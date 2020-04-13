edf4fc987eaed8a5dbabaf34ffb9f99c439c74de
Josh Mangiola <joshm982@gmail.com>
2020-04-12 22:03:51 -0400

    WIP AssetManager
    Starts work on the AssetManager

---
+++ src/asset_manager.h
@@ -0,0 +1,22 @@
+#ifndef ASSET_MANAGER_H
+#define ASSET_MANAGER_H
+#include <map>
+#include<SFML/Graphics.hpp>
+
+class AssetManager {
+public:
+    AssetManager() {}
+    ~AssetManager() {}
+
+    void LoadTexture(std::string name, std::string fileName);
+    sf::Texture &GetTexture(std::string name);
+
+    void LoadFont(std::string name, std::string fileName);
+    sf::Font &GetFont(std::string name);
+
+private:
+    std::map<std::string, sf::Texture> _textures;
+    std::map<std::string, sf::Font> _fonts;
+};
+
+#endif // ASSET_MANAGER_H
