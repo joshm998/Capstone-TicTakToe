c5234217c7efd7262a37ac44fdef693ffd860968
Josh Mangiola <joshm982@gmail.com>
2020-04-12 21:50:30 -0400

    State and StateMachine
    Adds the State and StateMachine classes to the project.

---
+++ src/state.h
@@ -0,0 +1,14 @@
+#ifndef STATE_H
+#define STATE_H
+
+class State {
+public:
+    virtual void Init() = 0;
+    virtual void HandleInput() = 0;
+    virtual void Update() = 0;
+    virtual void Draw(float deltaTime) = 0;
+    virtual void Pause() {}
+    virtual void Resume() {}
+};
+
+#endif // STATE_H
