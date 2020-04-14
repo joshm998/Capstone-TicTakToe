af5c0a2f7d13e1de1a689c324fbcea1edaa081c3
Josh Mangiola <joshm982@gmail.com>
2020-04-13 21:42:14 -0400

    Adds a Grid
    Adds a Grid to the Gameplay screen

--- src/game_state.h
+++ src/game_state.h
@@ -17,9 +17,14 @@ public:
     void Draw(float deltaTime) override;
 
 private:
+    void InitGridPieces();
     GameDataRef _data;
     sf::Sprite _background;
     sf::Sprite _pauseButton;
+    sf::Sprite _gridSprite;
+    sf::Sprite _gridPieces[3][3];
+    int gridArray[3][3];
+
     int turn;
     int gameState;
 };
